1f8ec2d666d11f89fe3d4dd766b3638d9fb7091b
Maxim Zadorkin <m.a.zadorkin@gmail.com>
2020-05-25 16:00:36 +0300

    add all

--- Laba4/src/FractalExplorer.java
+++ Laba4/src/FractalExplorer.java
@@ -1,68 +1,94 @@
 import java.awt.geom.Rectangle2D;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
+class FractalExplorer {
+    private int dimensionDisplay;
+    private JImageDisplay image;
+    private FractalGenerator generator;
+    private Rectangle2D.Double complexRange;
 
-/**
- * This class provides the common interface and operations for fractal
- * generators that can be viewed in the Fractal Explorer.
- */
-public abstract class FractalGenerator {
-
-    /**
-     * This static helper function takes an integer coordinate and converts it
-     * into a double-precision value corresponding to a specific range.  It is
-     * used to convert pixel coordinates into double-precision values for
-     * computing fractals, etc.
-     *
-     * @param rangeMin the minimum value of the floating-point range
-     * @param rangeMax the maximum value of the floating-point range
-     *
-     * @param size the size of the dimension that the pixel coordinate is from.
-     *        For example, this might be the image width, or the image height.
-     *
-     * @param coord the coordinate to compute the double-precision value for.
-     *        The coordinate should fall in the range [0, size].
-     */
-    public static double getCoord(double rangeMin, double rangeMax,
-        int size, int coord) {
-
-        assert size > 0;
-        assert coord >= 0 && coord < size;
-
-        double range = rangeMax - rangeMin;
-        return rangeMin + (range * (double) coord / (double) size);
+    public static void main() {
+        FractalExplorer start = new FractalExplorer(800);
+        start.createAndShowGUI();
+        start.drawFractal();
     }
 
-
-    /**
-     * Sets the specified rectangle to contain the initial range suitable for
-     * the fractal being generated.
-     */
-    public abstract void getInitialRange(Rectangle2D.Double range);
-
-
-    /**
-     * Updates the current range to be centered at the specified coordinates,
-     * and to be zoomed in or out by the specified scaling factor.
-     */
-    public void recenterAndZoomRange(Rectangle2D.Double range,
-        double centerX, double centerY, double scale) {
-
-        double newWidth = range.width * scale;
-        double newHeight = range.height * scale;
-
-        range.x = centerX - newWidth / 2;
-        range.y = centerY - newHeight / 2;
-        range.width = newWidth;
-        range.height = newHeight;
+    public FractalExplorer(int dimension) {
+        dimensionDisplay = dimension;
+        complexRange = new Rectangle2D.Double();
+        generator = new Mandelbrot();
+        generator.getInitialRange(complexRange);
+        image = new JImageDisplay(dimensionDisplay, dimensionDisplay);
+        return;
     }
 
+    public void createAndShowGUI() {
+        image.setLayout(new BorderLayout());
 
-    /**
-     * Given a coordinate <em>x</em> + <em>iy</em> in the complex plane,
-     * computes and returns the number of iterations before the fractal
-     * function escapes the bounding area for that point.  A point that
-     * doesn't escape before the iteration limit is reached is indicated
-     * with a result of -1.
-     */
-    public abstract int numIterations(double x, double y);
+        // основная картинка в центер 
+        JFrame frame = new JFrame("FractalExp");
+        frame.add(image, BorderLayout.CENTER);
+        
+        JButton resetImageButton = new JButton("Reset Display");
+        
+        // кнопка вниз
+        Reset handler = new Reset();
+        resetImageButton.addActionListener(handler);
+        frame.add(resetImageButton, BorderLayout.SOUTH);
+
+        Mouse click = new Mouse();
+        image.addMouseListener(click);
+        
+        // кнопка выхода
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+        return;
+    } 
+
+    private void drawFractal() {
+        for (int x = 0; x < dimensionDisplay; x+= 1){
+            for (int y = 0; y < dimensionDisplay; y+= 1){
+                double xCoord = generator.getCoord(complexRange.x, complexRange.x + complexRange.width, dimensionDisplay, x);
+                double yCoord = generator.getCoord(complexRange.y, complexRange.y + complexRange.height, dimensionDisplay, y);
+                int numIters = generator.numIterations(xCoord, yCoord);
+
+                if (numIters == -1) {
+                    image.drawPixel(x, y, 0);
+                } else {
+                    float hue = 0.7f + (float) numIters / 200f;
+                    int rgbColor = Color.HSBtoRGB(hue, 1f, 1f);
+                    image.drawPixel(x, y, rgbColor);
+                }
+
+            }
+        }
+        image.repaint();
+    }
+
+    class Reset implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            generator.getInitialRange(complexRange);
+            drawFractal();
+        }
+    }
+
+    class Mouse extends MouseAdapter {
+        public void mouseClicked(MouseEvent e) {
+            int x = e.getX();
+            double xCoord = generator.getCoord(complexRange.x, complexRange.x + complexRange.width, dimensionDisplay, x);
+            
+            int y = e.getY();
+            double yCoord = generator.getCoord(complexRange.y, complexRange.y + complexRange.height, dimensionDisplay, y);
+            
+            generator.recenterAndZoomRange(complexRange, xCoord, yCoord, 0.5);
+            
+            drawFractal();
+        }
+    }
+
 }
